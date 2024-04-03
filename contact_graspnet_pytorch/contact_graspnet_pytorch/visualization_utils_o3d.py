cc66918854dfeacda78a6592eba366afebb630a3
apirrone <awanegenebayo@gmail.com>
2024-04-03 12:30:51 +0200

    fix

--- contact_graspnet_pytorch/contact_graspnet_pytorch/visualization_utils_o3d.py
+++ contact_graspnet_pytorch/contact_graspnet_pytorch/visualization_utils_o3d.py
@@ -1,6 +1,6 @@
 import os
 
-import contact_graspnet_pytorch.mesh_utils
+import contact_graspnet_pytorch.mesh_utils as mesh_utils
 import matplotlib.pyplot as plt
 import numpy as np
 import open3d as o3d
